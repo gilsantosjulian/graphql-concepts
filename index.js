3b10cdc1f22a4bbc397cc36302a065b84e950e62
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-05-05 23:27:45 -0500

    feat: add resolvers

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
   type Query {
     hello: String
+    saludo: String
   }
 `)
 
+// Resolvers configuration
+const resolvers = {
+  hello: () => {
+    return 'Hola mundo'
+  },
+  saludo: () => {
+    return 'Hola a todos'
+  }
+}
+
 // Hello Query execution
-graphql(schema, '{ hello }')
+graphql(schema, '{ saludo }', resolvers)
   .then(data => {
     console.log(data);
   })
+  
